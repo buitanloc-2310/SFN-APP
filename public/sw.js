b26be755df1ecc12b54273146d5257bc2397784d
buitanloc-2310 <buitanloccon@gmail.com>
2026-09-02 00:39:42 +0700

    Delete public directory

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='sfn-shell-2026-09-ui2';
-const SHELL=['/','/index.html','/styles.css','/app.js','/static-i18n.js','/manifest.webmanifest','/assets/sfn-logo.png','/assets/sfn-wordmark.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(e.request.method!=='GET'||u.pathname.startsWith('/api/')) return;
-  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))));
-});
